d3a855df0a855bd4c879f560a4fb7cf4a6a875b7
Arclipse <arctale.work@gmail.com>
2022-01-20 16:42:36 +0100

    Added error page for 404

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import Footer from '../components/footer';
+
+export default function Error_404() {
+    const router = useRouter()
+    return (
+        <div className='error_page'>
+
+            <div className='content'>
+                <h1>We couldn’t find what you were looking for.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et lectus eu tincidunt faucibus. Vel venenatis eget euismod nulla ut. eget euismod nulla ut. eget euismod nulla ut. eget euismod nulla ut.</p>
+                <button onClick={() => {router.push("/", "/", {scroll: false})}}>Back to Safety</button>
+            </div>
+            <Footer />
+        </div>
+    );
+}
