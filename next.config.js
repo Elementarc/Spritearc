dbb8d6bab3f2910a1e90ce563bfe5354237b8fe7
Arclipse <arctale.work@gmail.com>
2022-02-06 08:05:07 +0100

    backup

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "192.168.178.21", "0.0.0.0", "https://35.207.80.151"]
+    domains: ["localhost", "192.168.178.21", "0.0.0.0", "https://35.207.80.151/packs", "https://35.207.80.151/profile_pictures", "https://35.207.80.151/profile_banners"]
   },
 }
 
